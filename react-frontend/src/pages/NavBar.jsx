fa78e6859d785e5e4e9ddb7d27529d0e9f94f280
viggnah <viggnahasd@gmail.com>
2024-03-19 11:16:23 +0530

    Small UI adjustment for Home page

--- react-frontend/src/pages/NavBar.jsx
+++ react-frontend/src/pages/NavBar.jsx
@@ -65,11 +65,11 @@ const Navigation = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand>FinApp</Navbar.Brand>
+                <Nav.Link as={Link} to="/"><Navbar.Brand>FinApp</Navbar.Brand></Nav.Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        {/* <Nav.Link as={Link} to="/">Home</Nav.Link> */}
                         {<Nav.Link as={Link} to="/acctandtxns">Acct & Txns</Nav.Link >}
                     </Nav>
                 </Navbar.Collapse>
