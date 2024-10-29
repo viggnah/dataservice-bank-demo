b0892f688743287be5c97260291ec60d7d7f58d1
viggnah <viggnahasd@gmail.com>
2024-10-29 12:57:43 +0530

    Minor edit

--- react-frontend/src/constants/config.js
+++ react-frontend/src/constants/config.js
@@ -2,11 +2,11 @@
 export const AsgardeoConfig = {
     signInRedirectURL: "http://localhost:3000",
     signOutRedirectURL: "http://localhost:3000",
-    clientID: "NcCfjP0F0dBYqrsxJbUefAfBUjEa",
-    // clientSecret: "M8XUEOdmLA5RZ9lNxaoEoYhN2YnL51UbfVtdXypfZjca",
-    // clientID: "kijZfyncfVZsNtYR6vt4aHFM3osa",
+    clientID: "",
+    // clientSecret: "",
+    // clientID: "",
     baseUrl: "https://localhost:9443",
-    // baseUrl: "https://api.asgardeo.io/t/viggnah",
+    // baseUrl: "https://api.asgardeo.io/t/<org>",
     scope: ["openid", "profile", "groups", "roles"]
 };
 
@@ -27,7 +27,7 @@ export const Hosts = {
 
 };
 
-export const AccessToken = "eyJ4NXQiOiJNek5rWmpVM1ltWmhaRGRpTkRabVpHVTJabVJsT1RoaE9XVXpOV0UzTWpRNFpERmpOV1k0TXciLCJraWQiOiJPREJtTVRVMFpqSmpPREprTkdZMVpUaG1ZamsyWVRZek56UmpZekl6TVRCbFlqRTBNV0prWTJJeE5qZzNPRGRqWVdRNVpXWmhOV0kwTkRBM1pqTTROUV9SUzI1NiIsInR5cCI6ImF0K2p3dCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzMWMzYTY3Mi00NWUyLTRkOTAtYjE1OS0xZmJhOTNlNDI5M2MiLCJhdXQiOiJBUFBMSUNBVElPTiIsImF1ZCI6ImREWXc1cG1SbENiTWVLQjRqR2ZMRDE3Unhzb2EiLCJuYmYiOjE3MzAxMjE1NjIsImF6cCI6ImREWXc1cG1SbENiTWVLQjRqR2ZMRDE3Unhzb2EiLCJzY29wZSI6ImRlZmF1bHQiLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo5NTAwL29hdXRoMi90b2tlbiIsImV4cCI6MTczNTMwNTU2MiwiaWF0IjoxNzMwMTIxNTYyLCJqdGkiOiI3YTBjYTRjYi1kNWQ5LTRjZDYtYTBhYi0xMTg5MjJkODQzODQiLCJjbGllbnRfaWQiOiJkRFl3NXBtUmxDYk1lS0I0akdmTEQxN1J4c29hIn0.AQJqUvj8MrSXQ-KRBqNCDWz_U2GPyAuk_WgnaAnXBv0cCSKIHHqyhg2nCmPDDJO95A9aUM9jWWEpnje2zpTMBVSuqdTt0d1eaAZpkUjSp9NxmO4NAwqiCA4cn44qELKm4KJ2nltxWOJvpiR4R1G3RuSEYfmbB242yoN3v6csErpwUJsbGlE-0vEMontGweTrJIjQ-ZTnDXWBDHKtdPwtAshGUl_dFv-qRw1cK5Dln_4efy1buGQh2hng1UqAEW9dpi0BuXlExvWeXpnRUEecYMvpdu2yn0_ebXnj48X_hZlIi2LfdLpRzPYqk9YVqDPDvfC4mypvi8uTaIGH00BzBA"
+export const AccessToken = ""
 
 
 
